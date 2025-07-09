631b2843454374be15d0d75d2c885d4c81b615d4
NicknEma <emanuele.rovini@gmail.com>
2025-07-09 18:40:14 +0200

    Use X macros to define type enum

--- src/el_type.h
+++ src/el_type.h
@@ -1,18 +1,29 @@
 #ifndef EL_TYPE_H
 #define EL_TYPE_H
 
+#define TYPE_KIND_NAMES \
+X(TYPE_UNKNOWN) \
+X(TYPE_VOID) \
+X(TYPE_TYPE) \
+X(TYPE_BOOLEAN) \
+X(TYPE_INTEGER) \
+X(TYPE_STRING) \
+X(TYPE_STRUCT) \
+X(TYPE_POINTER) \
+X(TYPE_PROC) \
+X(TYPE_COUNT)
+
 typedef enum Type_Kind {
-	TYPE_UNKNOWN = 0,
-	TYPE_VOID,
-	TYPE_TYPE,
-	TYPE_BOOLEAN,
-	TYPE_INTEGER,
-	TYPE_STRING,
-	TYPE_STRUCT,
-	TYPE_POINTER,
-	TYPE_PROC,
-	TYPE_COUNT,
+#define X(name) name,
+	TYPE_KIND_NAMES
+#undef  X
 } Type_Kind;
+
+global read_only String type_kind_names[] = {
+#define X(name) string_from_lit_const(#name),
+	TYPE_KIND_NAMES
+#undef  X
+};
 
 typedef struct Type Type;
 struct Type {
@@ -35,18 +46,5 @@ struct Type_Array {
 	i64    count;
 };
 
-
-global read_only String type_kind_names[] = {
-	string_from_lit_const("TYPE_UNKNOWN"),
-	string_from_lit_const("TYPE_VOID"),
-	string_from_lit_const("TYPE_TYPE"),
-	string_from_lit_const("TYPE_BOOLEAN"),
-	string_from_lit_const("TYPE_INTEGER"),
-	string_from_lit_const("TYPE_STRING"),
-	string_from_lit_const("TYPE_STRUCT"),
-	string_from_lit_const("TYPE_POINTER"),
-	string_from_lit_const("TYPE_PROC"),
-	string_from_lit_const("TYPE_COUNT"),
-};
 
 #endif
